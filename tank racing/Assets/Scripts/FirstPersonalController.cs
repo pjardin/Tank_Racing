1fd07a4b708536dcc4b194d05281ccbfb7c7ac1e
MDDeatley <MDDeatley@users.noreply.github.com>
2020-04-15 20:10:30 -0700

    Better Tank camera, controls, and added effects

--- tank racing/Assets/Scripts/FirstPersonalController.cs
+++ tank racing/Assets/Scripts/FirstPersonalController.cs
@@ -9,154 +9,208 @@ public class FirstPersonalController : MonoBehaviour
     private Rigidbody rbody;    //tanks rigid body
     //public LayerMask ground;
 
-    public float CurSpeed;  //current speed of tank in some units
-    public float SpeedLimit = 150; //max speed of tank in some units
-    public float MaxLimit = 150;    //maximum speed limit
-    public float MinLimit = 40; //minimum speed limit
-    //public GameObject explosionPrefab;
+
+    //Movement
+    public float curSpeed;  //current speed of tank in some units
+    public float speedLimit = 40; //max speed of tank in some units
+    public float maxLimit = 150;    //maximum speed limit
+    public float minLimit = 30; //minimum speed limit
+    public float rotationSpeed = .75f;   //applies multiplier to track speed in opposite directions (can be any number, recomend between 0 and 1, negative inverts controls)
+    public float reverseSpeed = .66f;   //applies multiplier to track speed in when in reverse (can be any number, recomend between 0 and 1, negative means tank cant reverse)
+    public float accel = 1000f; //tank acceleration, value is the torque applied to the tread contact wheel in newtons, the tanks has a lot of mass (negative inverts controls)
+
+    //Camera
+    public GameObject camPivot;
+    public float camRot = 500;
 
     //Body/Tread Stuff
     private Vector3 direction;  //tank move input storage
-    public Transform TreadL;    //tank tread gameobject containging WheelColliders
-    public Transform TreadR;    //tank tread gameobject containging WheelColliders
-    public float RotationSpeed = .75f;   //applies multiplier to track speed in opposite directions (can be any number, recomend between 0 and 1, negative inverts controls)
-    public float ReverseSpeed = .66f;   //applies multiplier to track speed in when in reverse (can be any number, recomend between 0 and 1, negative means tank cant reverse)
-    public float Accel = 1000f; //tank acceleration, value is the torque applied to the tread contact wheel in newtons, the tanks has a lot of mass (negative inverts controls)
-    //public float brakespeed = 1000f; //tank breaks, value is the torque applied to the tread contact wheel in newtons, when not accelerating, recommend same as accel at the moment
+    public Transform treadL;    //tank tread gameobject containging WheelColliders
+    public Transform treadR;    //tank tread gameobject containging WheelColliders
 
     //Turret Stuff
-    private Vector3 Turretdirection;    //turret rotation input storage
-    public GameObject Turret;   //turret body object
-    public GameObject Barrel;   //turret barrel object
+    private Vector3 turretDirection;    //turret rotation input storage
+    public GameObject turret;   //turret body object
+    public GameObject barrel;   //turret barrel object
     private float trotationY = 0f;  //y rotation of turret
     private float trotationX = 0f;  //x rotation of turret
     public float turretRot = 1.0f;  //turret rotation speed, 
     private float turRotLimX = 100f;    //turret left/right turn limit
-    private float turRotLimYup = 32f;   //turret up limit in degrees
-    private float turRotLimYdn = -11f;  //turret down limit in degrees
+    private float turRotLimYUp = 32f;   //turret up limit in degrees
+    private float turRotLimYDn = -11f;  //turret down limit in degrees
 
     //Bullet Stuff
     public Transform bulletSpawn;   //shell spawn location
     public GameObject bulletPrefab; //shell object to be launched
     private float bulletSpeed = 100;    //speed shells are thrown
+    public float recoil = 250000;
+    private float cooldown = 0;
+    public float fireRate = 1;
+    public int maxAmmo = 50;
+    public int ammo = 50;
 
     //Audio Stuff
-    //private AudioSource Engineaudio;  //self explanitory
-    //private AudioSource Fireaudio;    //audio for firing a shell
-    //private AudioSource Explosionaudio; //audio for shell exploding
+    //private AudioSource engineAudio;  //self explanitory
+    //private AudioSource treadAudio;  //self explanitory
+    //private AudioSource fireAudio;    //audio for firing a shell
+    //private AudioSource explosionAudio; //audio for shell exploding
 
+    //Special Effects
+    private bool Ldust = false;
+    public ParticleSystem dustSpawnLr;
+    public ParticleSystem dustSpawnLf;
+    private bool Rdust = false;
+    public ParticleSystem dustSpawnRr;
+    public ParticleSystem dustSpawnRf;
+    //public GameObject exhaust;
+    public GameObject muzzle;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         //jumpHeight = 5.0f; // tanks cant jump, but...
         rbody = GetComponent<Rigidbody>();
-        //audio = GetComponent<AudioSource>();
-        
+        //audio = GetComponent<AudioSource>();   
+        dustSpawnLf.Stop();
+        dustSpawnLr.Stop();
+        dustSpawnRf.Stop();
+        dustSpawnRr.Stop();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
+    private void Update() {
         direction = Vector3.zero;
         direction.x = Input.GetAxisRaw("Vertical"); //GetAxisRaw  is always -1, 0, or 1 so no input lag
         direction.z = Input.GetAxisRaw("Horizontal");
-        Turretdirection = Vector3.zero;
-        Turretdirection.x = Input.GetAxis("Horizontal2");
-        Turretdirection.y = Input.GetAxis("Vertical2");
+        turretDirection = Vector3.zero;
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            float f = Mathf.MoveTowardsAngle(camPivot.transform.localEulerAngles.y, trotationX, camRot * Time.deltaTime);
+            camPivot.transform.localEulerAngles = new Vector3(0, f, 0);
+        } else {
+
+            float f = Mathf.MoveTowardsAngle(camPivot.transform.localEulerAngles.y, 0, camRot * Time.deltaTime);
+            camPivot.transform.localEulerAngles = new Vector3(0, f, 0);
+        }
+            turretDirection.x = Input.GetAxis("Horizontal2");
+            turretDirection.y = Input.GetAxis("Vertical2");
 
         //Forward/Rotational movement
-        if (direction.x < 0) { direction.x = direction.x * ReverseSpeed; } // if reversing apply reverse speed mutiplier
-        CurSpeed = rbody.velocity.magnitude * 3.6f;
+        if (direction.x < 0) { direction.x *= reverseSpeed; } // if reversing apply reverse speed mutiplier
+        curSpeed = rbody.velocity.magnitude * 3.6f;
+        float accelR = accel;
+        if (direction.x != 0) { accelR *= (direction.x); }  //apply direction to acceleration
+        if (direction.z != 0) { accelR *= (-rotationSpeed * direction.z); } // if turnig apply rotation speed mutiplier
+        //Debug.Log("direction: " + direction + ", curSpeed:" + curSpeed + " is stopped:" + dustSpawnRr.isStopped + " dust:" + ldust);
+        if ((direction != Vector3.zero) && (curSpeed >= 3.0f) && !Rdust) {
+            Rdust = true;
+            dustSpawnRr.Play();
+            dustSpawnRf.Play(); 
+        } else if (direction == Vector3.zero && curSpeed < 3.0f && Rdust) {
+            Rdust = false;
+            dustSpawnRr.Stop();
+            dustSpawnRf.Stop(); 
+        }
 
-        float AccelR = Accel;
-        if (direction.x != 0) { AccelR = AccelR * (direction.z); }  //apply direction to acceleration
-        if (direction.z != 0) { AccelR = AccelR * (-RotationSpeed * direction.z); } // if turnig apply rotation speed mutiplier
-        if (CurSpeed > SpeedLimit && direction.z == 0) { AccelR = 0; } //if above speed limit and not turing, motors apply 0 torque
-
-        float AccelL = Accel;
-        if (direction.x != 0) { AccelL = AccelL * (direction.z); }
-        if (direction.z != 0) { AccelL = AccelL * (RotationSpeed * direction.z); } // if turnig apply rotation speed mutiplier opposite of right tread
-        if (CurSpeed > SpeedLimit && direction.z == 0) { AccelL = 0; }
+        if (curSpeed > speedLimit && direction.z == 0) { accelR = 0; } //if above speed limit and not turing, motors apply 0 torque
         
-        foreach (Transform contact in TreadR) { //for each WheelCollider in TreadR apply torque in direction to tank, this is really janky, need to adjust traction on wheelcollision objects
+
+        float accelL = accel;
+        if (direction.x != 0) { accelL *= (direction.x); }
+        if (direction.z != 0) { accelL *= (rotationSpeed * direction.z); } // if turnig apply rotation speed mutiplier opposite of right tread        pS.enableEmission = true;
+        
+        if (direction != Vector3.zero && curSpeed >= 3.0f && !Ldust) {
+            Ldust = true;
+            dustSpawnLr.Play(); 
+            dustSpawnLf.Play(); 
+        } else if (direction == Vector3.zero && curSpeed < 3.0f && Ldust) {
+            Ldust = false;
+            dustSpawnLr.Stop(); 
+            dustSpawnLf.Stop(); 
+        }
+
+        if (curSpeed > speedLimit && direction.z == 0) { accelL = 0; }
+
+        foreach (Transform contact in treadR) { //for each WheelCollider in TreadR apply torque in direction to tank, this is really janky, need to adjust traction on wheelcollision objects
             WheelCollider w = contact.GetComponent<WheelCollider>();
             if (direction != Vector3.zero) {
                 w.brakeTorque = 0;
-                w.motorTorque = AccelR;
+                w.motorTorque = accelR;
             } else {
-                w.brakeTorque = Accel;
+                w.brakeTorque = accel;
                 w.motorTorque = 0;
             }
         }
 
-        foreach (Transform contact in TreadL) {
+        foreach (Transform contact in treadL) {
             WheelCollider w = contact.GetComponent<WheelCollider>();
             if (direction != Vector3.zero) {
                 w.brakeTorque = 0;
-                w.motorTorque = AccelL;
+                w.motorTorque = accelL;
             } else {
-                w.brakeTorque = Accel;
+                w.brakeTorque = accel;
                 w.motorTorque = 0;
-            }  
+            }
         }
 
         //Turret rotation
-        if (Turretdirection.x != 0 || Turretdirection.y != 0) {
-            trotationX += Turretdirection.x * turretRot;
-            trotationY += Turretdirection.y * turretRot;
+        if (turretDirection.x != 0 || turretDirection.y != 0) {
+            trotationX += turretDirection.x * turretRot;
+            trotationY += turretDirection.y * turretRot;
             
             //limit x turret rotation
             if (trotationX > turRotLimX) {
                 trotationX = turRotLimX;
             } else if (trotationX < -turRotLimX) {
-                trotationX = -turRotLimX; 
+                trotationX = -turRotLimX;
             }
 
             //limit y turret rotation
-            if (trotationY > -turRotLimYdn) {
-                trotationY = -turRotLimYdn;
-            } else if (trotationY < -turRotLimYup) {
-                trotationY = -turRotLimYup;
+            if (trotationY > -turRotLimYDn) {
+                trotationY = -turRotLimYDn;
+            } else if (trotationY < -turRotLimYUp) {
+                trotationY = -turRotLimYUp;
             }
         }
 
-        Turret.transform.localEulerAngles = new Vector3(0, trotationX, 0);  //apply rotation to turret body
-        Barrel.transform.localEulerAngles = new Vector3(trotationY, 0, 0);  //apply rotation to turret barrel
+        turret.transform.localEulerAngles = new Vector3(0, trotationX, 0);  //apply rotation to turret body
+        barrel.transform.localEulerAngles = new Vector3(trotationY, 0, 0);  //apply rotation to turret barrel
 
         //Fire!!!
-        if (Input.GetButtonDown("Fire1")) {
-            Debug.Log("fire");
-            //audio.Play();
+        if (Input.GetButtonDown("Fire1") && Time.time > cooldown && ammo > 0) {
+            cooldown = Time.time + fireRate;
+            ammo--;
             Fire();
         }
-
     }
 
-    void OnCollisionEnter(Collision collision )
-    {
-        if (collision.gameObject.tag == "Russian Water") {
-            if (SpeedLimit < MaxLimit) {
-                SpeedLimit = SpeedLimit + 10; 
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Russian Water")) {
+            if (speedLimit < maxLimit) {
+                speedLimit += 10;
             }
-            Destroy(collision.gameObject); 
+            Destroy(collision.gameObject);
         }
-        if (collision.gameObject.tag == "Regular Water")
-        {
-            if (SpeedLimit > MinLimit)
-            {
-                SpeedLimit = SpeedLimit - 10;
+        if (collision.gameObject.tag == "Regular Water") {
+            if (speedLimit > minLimit) {
+                speedLimit -= 10;
+            }
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag == "Ammo") {
+            ammo += 10;
+            if (ammo > maxAmmo) {
+                ammo = maxAmmo;
             }
             Destroy(collision.gameObject);
         }
     }
 
-    void Fire() {
-    	var bullet = (GameObject)Instantiate(
-    		bulletPrefab,
-    		bulletSpawn.position,
-    		bulletSpawn.rotation);
-    	bullet.GetComponent<Rigidbody>().velocity=bullet.transform.up * bulletSpeed;
-    	Destroy(bullet,5.0f);
+    private void Fire() {
+        GameObject bullet = Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.up * bulletSpeed;
+        rbody.AddForceAtPosition(-bulletSpawn.up * recoil, bulletSpawn.transform.position);
+        Destroy(bullet, 5.0f);
     }
 }
---
+++ tank racing/Assets/Scripts/ShellScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellScript : MonoBehaviour
+{
+    public GameObject shellExplode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        GameObject explosion = Instantiate(
+            shellExplode,
+            transform.position,
+            transform.rotation);
+        Destroy(explosion, 1.0f);
+        Destroy(gameObject);
+    }
+}
