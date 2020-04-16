c74eb9bf289d43b722a572189c45881f6ee48e72
Pascal Jardin <pjardin@me.com>
2020-04-16 15:22:58 -0700

    Got maze generator working!
    Got maze generator working!

--- tank racing/Assets/Scripts/MapGenerator.cs
+++ tank racing/Assets/Scripts/MapGenerator.cs
@@ -9,6 +9,24 @@ public class MapGenerator : MonoBehaviour {
 	public GameObject roadEND;
 	public GameObject roadFour;
 
+
+
+	public GameObject building1;
+	public GameObject building2;
+	public GameObject building3;
+	public GameObject building4;
+
+	public GameObject building5;
+	public GameObject building6;
+	public GameObject building7;
+	public GameObject building8;
+
+	public GameObject building9;
+	public GameObject building10;
+	public GameObject building11;
+	public GameObject building12;
+
+
 	//public LayerMask map;
 
 	public Vector2Int mapSize;
@@ -147,6 +165,101 @@ public class MapGenerator : MonoBehaviour {
 
 	}
 
+
+    void random_building(Vector3 where, int buildingRot)
+	{
+		Quaternion brot = Quaternion.identity;
+		brot.eulerAngles = new Vector3(0, buildingRot, 0);
+
+		int pick = Random.Range(1, 13);
+
+		switch (pick)
+		{
+			case 1:
+				Instantiate(building1, new Vector3(where.x, where.y, where.z), brot);
+
+				break;
+			case 2:
+				Instantiate(building2, new Vector3(where.x, where.y, where.z), brot);
+
+				break;
+
+			case 3:
+				Instantiate(building3, new Vector3(where.x, where.y, where.z), brot);
+
+				break;
+			case 4:
+				Instantiate(building4, new Vector3(where.x, where.y, where.z), brot);
+
+				break;
+			case 5:
+				Instantiate(building5, new Vector3(where.x, where.y, where.z), brot);
+
+				break;
+			case 6:
+				Instantiate(building6, new Vector3(where.x, where.y, where.z), brot);
+
+				break;
+			case 7:
+				Instantiate(building7, new Vector3(where.x, where.y, where.z), brot);
+
+				break;
+			case 8:
+				Instantiate(building8, new Vector3(where.x, where.y, where.z), brot);
+
+				break;
+
+			case 9:
+				Instantiate(building9, new Vector3(where.x, where.y, where.z), brot);
+
+				break;
+			case 10:
+				Instantiate(building10, new Vector3(where.x, where.y, where.z), brot);
+
+				break;
+			case 11:
+				Instantiate(building11, new Vector3(where.x, where.y, where.z), brot);
+
+				break;
+			default:
+				Instantiate(building12, new Vector3(where.x, where.y, where.z), brot);
+
+				break;
+		}
+
+
+
+	}
+
+	void building(Vector3 where)
+	{
+		int buildingRot = 90;
+
+		random_building(new Vector3(where.x * offset - 15, where.y * offset, where.z * offset + 15), buildingRot);
+
+		random_building(new Vector3(where.x * offset - 15, where.y * offset, where.z * offset), buildingRot);
+
+		random_building(new Vector3(where.x * offset - 15, where.y * offset, where.z * offset - 15), buildingRot);
+
+		buildingRot = 270;
+
+		random_building(new Vector3(where.x * offset + 15, where.y * offset, where.z * offset + 15), buildingRot);
+
+		random_building(new Vector3(where.x * offset + 15, where.y * offset, where.z * offset), buildingRot);
+
+		random_building(new Vector3(where.x * offset + 15, where.y * offset, where.z * offset - 15), buildingRot);
+
+		buildingRot = 0;
+
+		random_building(new Vector3(where.x * offset, where.y * offset, where.z * offset - 15), buildingRot);
+
+		buildingRot = 180;
+
+
+		random_building(new Vector3(where.x * offset, where.y * offset, where.z * offset + 15), buildingRot);
+
+	}
+
 	void printMaze(string[,] maze)
 	{
 
@@ -259,6 +372,56 @@ public class MapGenerator : MonoBehaviour {
 
     void mazeTo3D(string[,] maze)
 	{
+		int buildingRot = 270;
+
+		for (int i = -1; i < mapSize.y * 2; i++)
+		{
+
+			random_building(new Vector3(- 15, 0, 15 * i), buildingRot);
+
+		}
+
+		buildingRot = 90;
+
+		for (int i = -1; i < mapSize.y * 2; i++)
+		{
+
+			random_building(new Vector3(mapSize.x * 15 + 45, 0, 15 * i), buildingRot);
+
+		}
+
+
+		buildingRot = 180;
+
+		for (int i = 1; i < mapSize.x * 2 - 1; i++)
+		{
+
+			random_building(new Vector3(15 * i, 0, -15), buildingRot);
+
+		}
+
+		buildingRot = 0;
+
+		for (int i = -1; i < mapSize.x * 2; i++)
+		{
+
+			random_building(new Vector3(15 * i, 0, mapSize.y * 30), buildingRot);
+
+		}
+
+		
+		for (int i = 0; i < mapSize.y; i++)
+		{
+			for (int j = 0; j < mapSize.x; j++)
+			{
+				if (maze[j, i] == "B")
+				{
+					building(new Vector3(j, 0, i));
+
+				}
+			}
+		}
+        
 
 		for (int i = 0; i < mapSize.y; i++)
 		{
