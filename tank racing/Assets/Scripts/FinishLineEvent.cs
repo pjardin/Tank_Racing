acc3565fb61585868cfcabc3093907734148a825
Rudy <rodri833@cougars.csusm.edu>
2020-04-16 19:25:06 -0700

    Added a simple Finishe line event.

---
+++ tank racing/Assets/Scripts/FinishLineEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FinishLineEvent : MonoBehaviour
+{
+    public GameObject tank;
+    // Start is called before the first frame update
+    public UnityEngine.Events.UnityEvent FinishLine;
+    void OnTriggerEnter(Collider collision)
+    {
+
+        if (collision.gameObject==tank)
+        {
+            Debug.Log("tank Entered");
+            FinishLine.Invoke();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
