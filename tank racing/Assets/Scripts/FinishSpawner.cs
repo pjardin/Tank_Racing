1b7213865f79e897601e22e6a48d0b020ee5c6c3
Rudy <rodri833@cougars.csusm.edu>
2020-05-07 21:53:41 -0700

    Added gun compass, and park area.

---
+++ tank racing/Assets/Scripts/FinishSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class FinishSpawner : MonoBehaviour
+{
+    GameObject[] test;
+    // Start is called before the first frame update
+    public void MoveIt()
+    {
+        test = GameObject.FindGameObjectsWithTag("Ground");
+        Debug.Log(test);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
